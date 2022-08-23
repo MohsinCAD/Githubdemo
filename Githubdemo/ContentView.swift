ad1e5e77beeaa8cf6131c384ec3162c4d8bb515c
Mohsin <mohsin.ahmad@gmail.com>
2022-08-23 01:37:07 -0600

    Initial Commit

---
+++ Githubdemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Githubdemo
+//
+//  Created by mohsin ahmad on 2022-08-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Githubdemo/GithubdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubdemoApp.swift
+//  Githubdemo
+//
+//  Created by mohsin ahmad on 2022-08-23.
+//
+
+import SwiftUI
+
+@main
+struct GithubdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
